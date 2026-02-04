faedefd49f6a918c93345444bce3bc94b065bd60
luca <lucavelazquez13@gmail.com>
2026-02-03 21:29:25 -0300

    CRUD

--- springboot-aplicaciones/src/main/java/com/libreria/controllers/ControladorArticulo.java
+++ springboot-aplicaciones/src/main/java/com/libreria/controllers/ControladorArticulo.java
@@ -3,12 +3,21 @@ package com.libreria.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.libreria.DTOs.ArticuloDTO;
 import com.libreria.services.ServicioArticulo;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
 
 @RestController
 @RequestMapping("/api/articulo")
@@ -20,5 +29,25 @@ public class ControladorArticulo {
     @GetMapping
     public List<ArticuloDTO> listar(){
         return servicio.consulta();
+    }
+
+    @GetMapping("/{id}")
+    public ArticuloDTO buscarPorId(@PathVariable Long id){
+        return servicio.buscarPorId(id);
+    }
+    
+    @PostMapping
+    public ArticuloDTO insertarArticulo(@RequestBody ArticuloDTO dto){
+        return servicio.insertarArticulo(dto);
+    }
+
+    @PutMapping("/{id}")
+    public ArticuloDTO putMethodName(@PathVariable Long id, @RequestBody ArticuloDTO dto) {
+        return servicio.actualizarArticulo(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public StringBuilder eliminarArticulo(@PathVariable Long id){
+        return servicio.eliminarArticulo(id);
     }
 }
--- springboot-aplicaciones/src/main/java/com/libreria/services/ServicioArticulo.java
+++ springboot-aplicaciones/src/main/java/com/libreria/services/ServicioArticulo.java
@@ -1,12 +1,14 @@
 package com.libreria.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.libreria.DTOs.ArticuloDTO;
+import com.libreria.entity.Articulo;
 import com.libreria.mapper.ArticuloMapper;
 import com.libreria.repositories.RepoArticulo;
 
@@ -19,4 +21,43 @@ public class ServicioArticulo {
         return repositorio.findAll().stream().map(ArticuloMapper::toDTO)
         .collect(Collectors.toList());
     }
+
+    public ArticuloDTO buscarPorId(Long id) {
+    Articulo articulo = repositorio.findById(id).orElseThrow();
+    return ArticuloMapper.toDTO(articulo);
 }
+
+
+    public ArticuloDTO insertarArticulo(ArticuloDTO dto){
+        Articulo articulo = ArticuloMapper.toEntity(dto);
+        Articulo insertado = repositorio.save(articulo);
+        return ArticuloMapper.toDTO(insertado);
+    }
+
+    public ArticuloDTO actualizarArticulo(Long id, ArticuloDTO dto){
+        Optional<Articulo> existe = repositorio.findById(id);
+        if(existe.isPresent()){
+            Articulo articulo = existe.get();
+            articulo.setNombre(dto.getNombre());
+            articulo.setPrecio(dto.getPrecio());
+            articulo.setCantStock(dto.getCantStock());
+
+            Articulo actualizado = repositorio.save(articulo);
+            return ArticuloMapper.toDTO(actualizado);
+        }else{
+            throw new RuntimeException("Articulo no encuentrado con id:" + id);
+        }
+    }
+
+    public StringBuilder eliminarArticulo(Long id){
+        StringBuilder sb = new StringBuilder();
+        if (repositorio.existsById(id)){
+            repositorio.deleteById(id);
+            sb.append("Se elimino correctamente");
+        } else{
+            sb.append("No se encontro un articulo");
+        }
+        return sb;
+    }
+}
+
